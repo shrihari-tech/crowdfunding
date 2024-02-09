82f286f5bd9e17664a4e0f67a9253e70f86908a4
SHRIHARI BH <bhshrihari@gmail.com>
2024-02-09 15:45:05 +0530

    wallet connection

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -5,7 +5,7 @@ import {Logo, Menu} from "../Components/index";
 const NavBar = () => {
   const {currentAccount,connectWallet} = useContext(CrowdFundingContext);
   const [isMenuOpen,setIsMenuOpen] = useState(false);
-  const menuList = ["White Paper","Project", "Donation", "Member"];
+  const menuList = ["Home","About", "Campaigns", "Contact"];
   return(
     <div class="backgroundMain">
       <div class="px-4 py-5 max-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
@@ -48,6 +48,7 @@ const NavBar = () => {
             class="p-2 -mr-1 transition durationn-200 rounded focus:outline-none focus:shadow-outline" onClick={()=>setIsMenuOpen(true)}><Menu/>
             </button>
             {isMenuOpen && (
+              <>
               <div class="absolute top-0 left-0 w-full">
                 <div class="p-5 bh-white border rounded shadow-sm">
                   <div class="flex items-center justify-between mb-4">
@@ -62,9 +63,53 @@ const NavBar = () => {
                       </a>
                     </div>
                   </div>
+                  <button aria-label="Close Menu"
+                  title = "Close Menu"
+                  class="p-2 -mt-2 -mr-2 transition duration-200 rounded
+                  hover:bg-gray-200 focus:bg-gray-200 focus:outline-none
+                  focus:shadow-outline"
+                  onClick={()=>setIsMenuOpen(false)}
+                  >
+                    <svg class="w-5 text-gray-600" viewBox="0 0 24 24">
+                      <path fill="currentColor"
+                      d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.
+                      4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,
+                      0,1.4C4.5,10.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.316.3,6.
+                      3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,
+                      0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
+                      />
+                    </svg>
+                  </button>
                 </div>
               </div>
-
+              <nav>
+              <ul class="space-y-4">
+                {menuList.map((el,i)=>(
+                  <li key={i+1}>
+                    <a href="/"
+                    aria-label="Our Product"
+                    title = "Our Product"
+                    class="font-medium tracking-wide text-gray-700
+                    transition-colors duration-200
+                    hover:text-deep-purple-accent-400"
+                    >{el}
+                    </a>
+                  </li>
+                ))}
+                <li>
+                  <a href="/"
+                  class="inline-flex items-center background justify-center 
+                  w-full h-12 px-6 font-medium tracking-wide text-wide 
+                  transition duration-200 rounded shadow-md 
+                  bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700
+                  focus:shadow-outline focus:outline-none"
+                  arial-label="Sign up"
+                  title="Sign up"></a>
+                </li>
+              </ul>
+            </nav>
+            </>
+            //need to check this gettinf error from nav
             )}
           </div>
         </div>
